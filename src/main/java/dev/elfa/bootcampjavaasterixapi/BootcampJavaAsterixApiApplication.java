04eaa553ce40ed5cce0a610ac972cc9b3426d361
René El Fatuaki <rene@elfatuaki.com>
2024-06-17 11:02:04 +0200

    init

---
+++ src/main/java/dev/elfa/bootcampjavaasterixapi/BootcampJavaAsterixApiApplication.java
@@ -0,0 +1,13 @@
+package dev.elfa.bootcampjavaasterixapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampJavaAsterixApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootcampJavaAsterixApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elfa/bootcampjavaasterixapi/BootcampJavaAsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elfa.bootcampjavaasterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampJavaAsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
